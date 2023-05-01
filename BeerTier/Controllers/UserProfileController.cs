bb56fb31b781c342668e5323926988a37e6acef4
Will Hedges <wshedges@outlook.com>
2023-05-01 12:08:18 -0500

    basic login working * added firebase project id to all appsettings (will fix later) * added login state handling to App.js * added Me() endpoint to userprofile controller * updated gitignore * removed appsettings from remote

--- BeerTier/Controllers/UserProfileController.cs
+++ BeerTier/Controllers/UserProfileController.cs
@@ -1,5 +1,6 @@
 ﻿using BeerTier.Models;
 using BeerTier.Repositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
--- BeerTier/client/src/App.js
+++ BeerTier/client/src/App.js
@@ -1,11 +1,34 @@
+import { useState, useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import logo from "./logo.svg";
 import "./App.css";
 import AppBar from "./AppBar";
 import ApplicationViews from "./views/ApplicationViews";
+import { onLoginStatusChange, me } from "./modules/authManager";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(null);
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    onLoginStatusChange(setIsLoggedIn);
+  }, []);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      me().then(setUserProfile);
+    } else {
+      setUserProfile(null);
+    }
+  }, [isLoggedIn]);
+
+  // The "isLoggedIn" state variable will be null until //  the app's connection to firebase has been established.
+  //  Then it will be set to true or false by the "onLoginStatusChange" function
+  if (isLoggedIn === null) {
+    // Until we know whether or not the user is logged in or not, just show a spinner
+    return <span>waiting...</span>;
+  }
   return (
     <Router>
       <AppBar />
