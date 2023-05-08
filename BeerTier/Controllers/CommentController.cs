1cd94f144fa426eb054e51ba8cc586bdfa7bf9af
Will Hedges <wshedges@outlook.com>
2023-05-08 11:38:00 -0500

    added Comment endpoints

---
+++ BeerTier/Controllers/CommentController.cs
@@ -0,0 +1,68 @@
+﻿using BeerTier.Models;
+using BeerTier.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
+
+namespace BeerTier.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentController : ControllerBase
+    {
+        private readonly ICommentRepository _commentRepository;
+        private readonly IUserProfileRepository _userProfileRepository;
+
+        public CommentController(
+            ICommentRepository commentRepository,
+            IUserProfileRepository userProfileRepository
+        )
+        {
+            _commentRepository = commentRepository;
+            _userProfileRepository = userProfileRepository;
+        }
+
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+            UserProfile userProfile = GetCurrentUserProfile();
+            comment.UserProfileId = userProfile.Id;
+            comment.CreateDateTime = DateTime.Now;
+            _commentRepository.Add(comment);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpPut]
+        public IActionResult Put(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+            _commentRepository.Update(comment);
+            return NoContent();
+        }
+
+        [Authorize]
+        [HttpDelete]
+        public IActionResult Delete(int id, Comment comment)
+        {
+            if (id != comment.Id)
+            {
+                return BadRequest();
+            }
+            _commentRepository.Delete(comment);
+            return NoContent();
+        }
+
+        private UserProfile GetCurrentUserProfile()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userProfileRepository.GetByFirebaseUserId(firebaseUserId);
+        }
+    }
+}
