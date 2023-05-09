a84553ae925382d4c34b15c82340b6faa34bfd9a
Will Hedges <wshedges@outlook.com>
2023-05-09 12:52:40 -0500

    Style edits, PUT call working * no validation yet, will probably go back and fix

--- BeerTier/client/src/components/Style.js
+++ BeerTier/client/src/components/Style.js
@@ -1,22 +1,63 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../UserContext";
 
-import { Box, Divider, ListItem, ListItemText } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  ListItem,
+  ListItemText,
+  TextField,
+} from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import CloseIcon from "@mui/icons-material/Close";
 import { DeleteIconButton } from "./DeleteButton";
 import { EditIconButton } from "./EditButton";
-import { deleteStyle } from "../modules/apiManager";
+import { deleteStyle, putObjToApi } from "../modules/apiManager";
 
 export default function Style({ style }) {
   const { userProfile } = useContext(UserContext);
 
+  const [editingStyle, setEditingStyle] = useState(false);
+  const [editedStyleName, setEditedStyleName] = useState("");
+
+  useEffect(() => {
+    setEditedStyleName(style.name);
+  }, [style.name, editingStyle]);
+
+  const handleEditedStyle = () => {
+    const editedStyleObj = {
+      id: style.id,
+      name: editedStyleName,
+    };
+    putObjToApi("style", editedStyleObj, style.id).then(() =>
+      window.location.reload(true)
+    );
+  };
+
   return (
     <Box sx={{ maxWidth: "300px" }}>
       <ListItem>
-        <ListItemText primary={style.name} />
-        {userProfile?.isAdmin && (
+        {!editingStyle ? (
+          userProfile?.isAdmin && (
+            <>
+              <ListItemText primary={style.name} />
+              <EditIconButton state={editingStyle} setter={setEditingStyle} />
+              <DeleteIconButton deleteCallback={deleteStyle} objRef={style} />
+            </>
+          )
+        ) : (
           <>
-            <EditIconButton />
-            <DeleteIconButton deleteCallback={deleteStyle} objRef={style} />
+            <TextField
+              value={editedStyleName}
+              onChange={(evt) => setEditedStyleName(evt.target.value)}
+            />
+            <IconButton onClick={handleEditedStyle}>
+              <CheckIcon />
+            </IconButton>
+            <IconButton onClick={() => setEditingStyle(false)}>
+              <CloseIcon />
+            </IconButton>
           </>
         )}
       </ListItem>
