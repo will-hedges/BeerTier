aba6f3709c1b693504db1fad1a28cf0fabe9bf25
Will Hedges <wshedges@outlook.com>
2023-05-09 11:44:28 -0500

    adding, deleting styles now working * see nested ternary, will want to go back and fix * style tweaks

--- BeerTier/client/src/components/Style.js
+++ BeerTier/client/src/components/Style.js
@@ -1,15 +1,24 @@
-import { Box, Divider, ListItem, ListItemText } from "@mui/material";
+import { useContext } from "react";
+import UserContext from "../UserContext";
 
+import { Box, Divider, ListItem, ListItemText } from "@mui/material";
 import { DeleteIconButton } from "./DeleteButton";
 import { EditIconButton } from "./EditButton";
+import { deleteStyle } from "../modules/apiManager";
 
 export default function Style({ style }) {
+  const { userProfile } = useContext(UserContext);
+
   return (
-    <Box sx={{ maxWidth: "200px" }}>
+    <Box sx={{ maxWidth: "300px" }}>
       <ListItem>
         <ListItemText primary={style.name} />
-        <EditIconButton />
-        <DeleteIconButton />
+        {userProfile?.isAdmin && (
+          <>
+            <EditIconButton />
+            <DeleteIconButton deleteCallback={deleteStyle} objRef={style} />
+          </>
+        )}
       </ListItem>
       <Divider />
     </Box>
--- BeerTier/client/src/pages/StylesPage.js
+++ BeerTier/client/src/pages/StylesPage.js
@@ -1,20 +1,79 @@
 import React, { useEffect, useState } from "react";
-import { Box, List, Typography } from "@mui/material/";
+import { Box, IconButton, List, TextField, Typography } from "@mui/material/";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 import Style from "../components/Style";
 
-import { getAll } from "../modules/apiManager";
+import { getAll, postObjToApi } from "../modules/apiManager";
 
 export default function StylesPage() {
   const [styles, setStyles] = useState([]);
+  const [newStyleName, setNewStyleName] = useState("");
+  const [validNewStyle, setValidNewStyle] = useState(true);
 
   useEffect(() => {
     getAll("style").then(setStyles);
   }, []);
 
+  useEffect(() => {
+    const styleNames = styles.map((style) => style.name);
+    if (styleNames.includes(newStyleName)) {
+      setValidNewStyle(false);
+    } else {
+      setValidNewStyle(true);
+    }
+  }, [styles, newStyleName]);
+
+  const handleNewStyleSubmit = () => {
+    postObjToApi("style", { name: newStyleName }).then(
+      window.location.reload(true)
+    );
+  };
   return (
     <Box sx={{ m: "1rem", p: "1rem" }}>
       <Typography variant="h4">Styles</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "flex-start",
+          m: "0.5rem",
+        }}
+      >
+        {/* TODO GO BACK AND FIX THIS NESTED TERNARY */}
+        {validNewStyle ? (
+          <>
+            {newStyleName ? (
+              <IconButton onClick={handleNewStyleSubmit}>
+                <AddCircleIcon />
+              </IconButton>
+            ) : (
+              <IconButton disabled>
+                <AddCircleIcon />
+              </IconButton>
+            )}
+            <TextField
+              label="New Style"
+              variant="standard"
+              sx={{ ml: "0.2rem" }}
+              onChange={(evt) => setNewStyleName(evt.target.value)}
+            />
+          </>
+        ) : (
+          <>
+            <IconButton disabled>
+              <AddCircleIcon color="error" />
+            </IconButton>
+            <TextField
+              error
+              helperText="This style already exists!"
+              label="New Style"
+              variant="standard"
+              sx={{ ml: "0.2rem" }}
+              onChange={(evt) => setNewStyleName(evt.target.value)}
+            />
+          </>
+        )}
+      </Box>
       <List>
         {styles.map((style) => (
           <Style key={`style--${style.id}`} style={style} />
